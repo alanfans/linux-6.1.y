dbac78714da375fc18d76373fb9c476d944384f2
alanfans <hylc550655145@msn.cn>
2023-04-11 15:27:49 +0800

    fix

--- drivers/soc/sunxi/sunxi_sram.c
+++ drivers/soc/sunxi/sunxi_sram.c
@@ -261,7 +261,7 @@ int sunxi_sram_claim(struct device *dev)
 }
 EXPORT_SYMBOL(sunxi_sram_claim);
 
-void sunxi_sram_release(struct device *dev)
+static void sunxi_sram_release(struct device *dev)
 {
 	const struct sunxi_sram_data *sram_data;
 	struct sunxi_sram_desc *sram_desc;
